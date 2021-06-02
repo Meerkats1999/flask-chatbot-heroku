b3038750d6f91c4b48b085b134455b4c086b314d
Abhrajyoti Pal <abhrajyoti1999@gmail.com>
2021-06-02 20:43:27 +0530

    again

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import time
 
 app = Flask(__name__);
 
-@app.route("/bot", methods=["POST"])
+@app.route("/bot", methods=["GET", "POST"])
 def response():
     query = dict(request.form)['query']
     res = query 
     return jsonify({"response" : res})
 
 if __name__=="__main__":
-    app.run(host="10.0.2.2",)
+    app.run(host="0.0.0.0",)
